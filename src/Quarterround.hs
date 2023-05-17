06ad0d11cec063cb7295d7d821f3640cd23fa33d
Alfredo Garcia <oxarbitrage@gmail.com>
2023-05-17 16:44:51 -0300

    add some code docs

--- src/Quarterround.hs
+++ src/Quarterround.hs
@@ -10,15 +10,23 @@ module Quarterround
 import Data.Bits
 import Data.Word
 
+-- Utility function to get the first positions of a 4-Tuple.
 get0 :: (Word32, Word32, Word32, Word32) -> Word32
 get0 (a, _, _, _) = a
+
+-- Utility function to get the second positions of a 4-Tuple.
 get1 :: (Word32, Word32, Word32, Word32) -> Word32
 get1 (_, a, _, _) = a
+
+-- Utility function to get the third positions of a 4-Tuple.
 get2 :: (Word32, Word32, Word32, Word32) -> Word32
 get2 (_, _, a, _) = a
+
+-- Utility function to get the fourth positions of a 4-Tuple.
 get3 :: (Word32, Word32, Word32, Word32) -> Word32
 get3 (_, _, _, a) = a
 
+-- The endofunctor
 data ExprF a = Const Word32
         | Mod a a
         | Rotl7 a
@@ -27,7 +35,7 @@ data ExprF a = Const Word32
         | Rotl18 a
         | Xor2 a a -- `2` needed to avoid ambiguity with Data.Bits.Xor
 
--- needed if no derive is done
+-- Functor instance. Needed if no automatic derive is done.
 instance Functor ExprF where
     fmap _ (Const i) = Const i
     fmap f (left `Mod` right) = f left `Mod` f right
@@ -37,13 +45,16 @@ instance Functor ExprF where
     fmap f (Rotl13 a) = Rotl13 (f a)
     fmap f (Rotl18 a) = Rotl18 (f a)
 
+-- Fix and unFix
 newtype Fix f = In (f (Fix f))
 unFix :: Fix f -> f (Fix f)
 unFix (In x) = x
 
+-- The catamorphism.
 cata :: Functor f => (f a -> a) -> Fix f -> a
 cata algebra = algebra . fmap (cata algebra) . unFix
 
+-- The algebra maps.
 alg :: ExprF Word32 -> Word32
 alg (Const i)   = i
 alg (a `Mod` b) = a + b
@@ -53,41 +64,51 @@ alg (Rotl13 a) = rotate a 13
 alg (Rotl18 a) = rotate a 18
 alg (a `Xor2` b) = xor a b
 
+-- The evaluator.
 eval :: Fix ExprF -> Word32
 eval = cata alg
 
+-- The right hand side of the `z1` expression as an expression. `((y0 + y3) <<< 7)`
 rhs1 :: (Word32, Word32, Word32, Word32) -> Fix ExprF
 rhs1 (y0, _, _, y3) = In $ 
     Rotl7 (In $ In (Const y0) `Mod` In (Const y3))
 
+-- The `z1` expression. `z1 = y1 ⊕ ((y0 + y3) <<< 7)`
 z1 :: (Word32, Word32, Word32, Word32) -> Fix ExprF
 z1 (y0, y1, y2, y3) = In $ 
   In (Const y1) `Xor2` rhs1 (y0, y1, y2, y3)
 
+-- The right hand side of the `z2` expression as an expression. `((z1 + y0) <<< 9)`
 rhs2 :: (Word32, Word32, Word32, Word32) -> Fix ExprF
 rhs2 (y0, y1, y2, y3) = In $ 
     Rotl9 (In $ z1 (y0, y1, y2, y3) `Mod` In (Const y0))
 
+-- The `z2` expression. `y2 ⊕ ((z1 + y0) <<< 9)`
 z2 :: (Word32, Word32, Word32, Word32) -> Fix ExprF
 z2 (y0, y1, y2, y3) = In $ 
     In (Const y2) `Xor2` rhs2 (y0, y1, y2, y3)
 
+-- The right hand side of the `z3` expression as an expression. `(z2 + z1) <<< 13)`
 rhs3 :: (Word32, Word32, Word32, Word32) -> Fix ExprF
 rhs3 (y0, y1, y2, y3) = In $ 
     Rotl13 (In $ z2 (y0, y1, y2, y3)  `Mod` z1 (y0, y1, y2, y3))
 
+-- The `z3` expression. `y3 ⊕ ((z2 + z1) <<< 13)`
 z3 :: (Word32, Word32, Word32, Word32) -> Fix ExprF
 z3 (y0, y1, y2, y3) = In $ 
     In (Const y3) `Xor2` rhs3 (y0, y1, y2, y3)
 
+-- The right hand side of the `z0` expression as an expression. `((z3 + z2) <<< 18)`
 rhs0 :: (Word32, Word32, Word32, Word32) -> Fix ExprF
 rhs0 (y0, y1, y2, y3) = In $ 
     Rotl18 (In $ z3 (y0, y1, y2, y3) `Mod` z2 (y0, y1, y2, y3))
 
+-- The `z0` expression. `y0 ⊕ ((z3 + z2) <<< 18)`
 z0 :: (Word32, Word32, Word32, Word32) -> Fix ExprF
 z0 (y0, y1, y2, y3) = In $ 
     In (Const y0) `Xor2` rhs0 (y0, y1, y2, y3)
 
+-- The quarterround expression. `quarterround(y) = (z0, z1, z2, z3)`
 quarterround :: (Word32, Word32, Word32, Word32) -> (Word32, Word32, Word32, Word32)
 quarterround (y0, y1, y2, y3) = (eval $ z0 (y0, y1, y2, y3), eval $ z1 (y0, y1, y2, y3), 
     eval $ z2 (y0, y1, y2, y3), eval $ z3 (y0, y1, y2, y3))
