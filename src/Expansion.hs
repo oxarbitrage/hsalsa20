37ec263759e6050988e13e12ec53868720e9dbb8
Alfredo Garcia <oxarbitrage@gmail.com>
2023-05-30 16:20:40 -0300

    add expansion function code

---
+++ src/Expansion.hs
@@ -0,0 +1,78 @@
+{-|
+Module      : Expansion
+Description : Salsa20 expansion function code
+Copyright   : (c) Alfredo Garcia, 2023
+License     : MIT
+Stability   : experimental
+Portability : POSIX
+
+...
+-}
+module Expansion
+    (
+    expand32,
+    expand16
+    ) where
+
+import Types (MatrixType, Matrix64Type, VectorType)
+import Data.Tuple.Select
+
+import Salsa20 (salsa20)
+
+-- |`expa` in ascii. 
+o0 :: VectorType
+o0 = (101, 120, 112, 97)
+
+-- |`nd 3` in ascii.
+o1 :: VectorType
+o1 = (110, 100, 32, 51)
+
+-- |`2-by` in ascii.
+o2 :: VectorType
+o2 = (50, 45, 98, 121)
+
+-- |`te k` in ascii.
+o3 :: VectorType
+o3 = (116, 101, 32, 107)
+
+-- |`expa` in ascii. 
+t0 :: VectorType
+t0 = (101, 120, 112, 97)
+
+-- |`nd 1` in ascii.
+t1 :: VectorType
+t1 = (110, 100, 32, 49)
+
+-- |`6-by` in ascii.
+t2 :: VectorType
+t2 = (54, 45, 98, 121)
+
+-- |`te k` in ascii.
+t3 :: VectorType
+t3 = (116, 101, 32, 107)
+
+-- |The expansion function where we have two 16 bytes k's (k0 and k1).
+expand32 :: MatrixType -> MatrixType -> MatrixType -> Matrix64Type
+expand32 k0 k1 n = salsa20 (sort32 k0 k1 n)
+
+-- |The order needed for the 32 bytes k version of the expansion function `expand32`.
+sort32 :: MatrixType -> MatrixType -> MatrixType -> Matrix64Type
+sort32 k0 k1 n = (
+    (o0, (sel1 k0), (sel2 k0), (sel3 k0)),
+    ((sel4 k0), o1, (sel1 n), (sel2 n)),
+    ((sel3 n), (sel4 n), o2, (sel1 k1)),
+    ((sel2 k1), (sel3 k1), (sel4 k1), o3)
+    )
+
+-- |The expansion function where we have one 16 bytes (k).
+expand16 :: MatrixType -> MatrixType -> Matrix64Type
+expand16 k n = salsa20 (sort16 k n)
+
+-- |The order needed for the 16 bytes k version of the expansion function `expand16`.
+sort16 :: MatrixType -> MatrixType -> Matrix64Type
+sort16 k n = (
+    (t0, (sel1 k), (sel2 k), (sel3 k)),
+    ((sel4 k), t1, (sel1 n), (sel2 n)),
+    ((sel3 n), (sel4 n), t2, (sel1 k)),
+    ((sel2 k), (sel3 k), (sel4 k), t3)
+    )
--- test/unit/fast/Spec.hs
+++ test/unit/fast/Spec.hs
@@ -3,6 +3,7 @@ import Rowround
 import Columnround
 import Doubleround
 import Salsa20
+import Expansion
 import Utils
 import Types
 
@@ -176,6 +177,31 @@ salsa20Output3 = (
     ((69, 144, 51, 57), (29, 29, 150, 26), (150, 30, 235, 249), (190, 163, 251, 48)), 
     ((27, 111, 114, 114), (118, 40, 152, 157), (180, 57, 27, 94), (107, 42, 236, 35)))
 
+-- Expanded Salsa20
+
+expandedk0 :: MatrixType
+expandedk0 = ((1, 2, 3, 4), (5, 6, 7, 8), (9, 10, 11, 12), (13, 14, 15, 16))
+
+expandedk1 :: MatrixType
+expandedk1 = ((201, 202, 203, 204), (205, 206, 207, 208), (209, 210, 211, 212), (213, 214, 215, 216))
+
+expandedn :: MatrixType
+expandedn = ((101, 102, 103, 104), (105, 106, 107, 108), (109, 110, 111, 112), (113, 114, 115, 116))
+
+expandedOutput1 :: Matrix64Type
+expandedOutput1 = (
+    ((69, 37, 68, 39), (41, 15, 107, 193), (255, 139, 122, 6), (170, 233, 217, 98)),
+    ((89, 144, 182, 106), (21, 51, 200, 65), (239, 49, 222, 34), (215, 114, 40, 126)),
+    ((104, 197, 7, 225), (197, 153, 31, 2), (102, 78, 76, 176), (84, 245, 246, 184)),
+    ((177, 160, 133, 130), (6, 72, 149, 119),(192, 195, 132, 236), (234, 103, 246, 74)))
+
+expandedOutput2 :: Matrix64Type
+expandedOutput2 = (
+    ((39, 173, 46, 248), (30, 200, 82, 17), (48, 67, 254, 239), (37, 18, 13, 247)),
+    ((241, 200, 61, 144), (10, 55, 50, 185), (6, 47, 246, 253), (143, 86, 187, 225)),
+    ((134, 85, 110, 246), (161, 163, 43, 235), (231, 94, 171, 51), (145, 214, 112, 29)),
+    ((14, 232, 5, 16), (151, 140, 183, 141), (171, 9, 122, 181), (104, 182, 177, 193)))
+
 main :: IO ()
 main = do
     putStrLn "Quarterround tests:"
@@ -224,6 +250,12 @@ main = do
     putStrLn $ if salsa20 salsa20Input1 == salsa20Output1 then "OK" else "FAIL!"
     putStrLn $ if salsa20 salsa20Input2 == salsa20Output2 then "OK" else "FAIL!"
     putStrLn $ if salsa20 salsa20Input3 == salsa20Output3 then "OK" else "FAIL!"
+
+    putStrLn ""
+
+    putStrLn "Expanded Salsa20 tests:"
+    putStrLn $ if expand32 expandedk0 expandedk1 expandedn == expandedOutput1 then "OK" else "FAIL!"
+    putStrLn $ if expand16 expandedk0 expandedn == expandedOutput2 then "OK" else "FAIL!"
 
     putStrLn ""
 
