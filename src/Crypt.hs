42b64aa32e855c7dac847d107090d52841bf31f9
Alfredo Garcia <oxarbitrage@gmail.com>
2023-09-27 09:45:56 -0300

    make improvments to the functions in the Crypt module

--- src/Crypt.hs
+++ src/Crypt.hs
@@ -36,68 +36,92 @@ floor of it is taken.
 The resulting  number is returned expressed as a unique 8 bytes sequence.
 -}
 iOver64 :: Integral a => a -> [Word32]
-iOver64 i = extractBytes 8 $ floor ((fromIntegral i / 64) :: Double)
+iOver64 index = extractBytes 8 $ floor (fromIntegral index / 64 :: Double)
 
 -- |Display the calculation of an index over 64. See `iOver64`.
 iOver64Display :: String -> [String]
-iOver64Display i = displayBytes 8 $ printf "_(%s/64)" i
+-- TODO: is this the same?
+--iOver64Display index = [printf "_(%s/64)" index]
+iOver64Display index = displayBytes 8 $ printf "_(%s/64)" index
 
 {- |Join the nonce and the calculated `iOver64`.
 Nonce is 8 bytes and index 8 bytes more for a total of 16 as the result.
 -}
 nonceAndiOver64 :: [Word32] -> Int -> [Word32]
-nonceAndiOver64 n i = n ++ iOver64 i
+nonceAndiOver64 nonce index
+    | length nonce == 8 = nonce ++ iOver64 index
+    | otherwise = error "First input to `nonceAndiOver64` must be a list of 8 `Word32` numbers"
 
 -- |Join the nonce and the calculated `iOver64Display`.
 nonceAndiOver64Display :: [String] -> String -> [String]
-nonceAndiOver64Display n i = n ++ iOver64Display i
+nonceAndiOver64Display nonce index
+    | length nonce == 8 = nonce ++ iOver64Display index
+    | otherwise = error "first input to `nonceAndiOver64Display` must be a list of 8 `String` strings"
 
 -- |Given a single 16 bytes key, a nonce and an index of a message byte, get the salsa20 expanded matrix of it. 
 cryptV1 :: [Word32] -> [Word32] -> Int -> [Word32]
-cryptV1 k n i = expand16Compute k $ nonceAndiOver64 n i
+cryptV1 key nonce index
+    | length key == 16 && length nonce == 8 = expand16Compute key $ nonceAndiOver64 nonce index
+    | otherwise = error "first input to `cryptV1` must be a list of 16 `Word32` numbers and the second a list of 8 `Word32` numbers"
 
 -- |Given a single 16 bytes key, a nonce and an index get the salsa20 expanded matrix of it.
 cryptV1Display :: [String] -> [String] -> String -> [String]
-cryptV1Display k n i = expand16Display k $ nonceAndiOver64Display n i
+cryptV1Display key nonce index
+    | length key == 16 && length nonce == 8 = expand16Display key $ nonceAndiOver64Display nonce index
+    | otherwise = error "first input to `cryptV1Display` must be a list of 16 `String` strings and the second a list of 8 `String` strings"
 
 -- |Given two 16 bytes keys, a nonce and an index get the salsa20 expanded matrix of it.
 cryptV2 :: [Word32] -> [Word32] -> [Word32] -> Int -> [Word32]
-cryptV2 k0 k1 n i = expand32Compute k0 k1 $ nonceAndiOver64 n i
+cryptV2 key0 key1 nonce index 
+    | length key0 == 16 && length key1 == 16 && length nonce == 8 = expand32Compute key0 key1 $ nonceAndiOver64 nonce index
+    | otherwise = error "first input to `cryptV2` must be a list of 16 `Word32` numbers, the second a list of 16 `Word32` numbers and the third a list of 8 `Word32` numbers"
 
 -- |Given two 16 bytes keys, a nonce and an index get the salsa20 expanded matrix of it.
 cryptV2Display :: [String] -> [String] -> [String] -> String -> [String]
-cryptV2Display k0 k1 n i = expand32Display k0 k1 $ nonceAndiOver64Display n i
+cryptV2Display key0 key1 nonce index
+    | length key0 == 16 && length key1 == 16 && length nonce == 8 = expand32Display key0 key1 $ nonceAndiOver64Display nonce index
+    | otherwise = error "first input to `cryptV2Display` must be a list of 16 `String` strings, the second a list of 16 `String` strings and the third a list of 8 `String` strings"
 
 -- |Given an aumented key and an index of it, returns the number corresponding to that index.
 keybyte :: [Word32] -> Int -> Word32
-keybyte aumented_key n = aumented_key!!n
+keybyte aumented_key index
+    | length aumented_key == 64 = aumented_key!!index
+    | otherwise = error "first input to `keybyte` must be a list of 64 `Word32` numbers"
 
 -- |Given an aumented key as a list of strings and an index of it, returns the string corresponding to that index.
 keybyteDisplay :: [String] -> Int -> String
-keybyteDisplay aumented_key n = aumented_key!!n
+keybyteDisplay aumented_key index
+    | length aumented_key == 64 = aumented_key!!index
+    | otherwise = error "first input to `keybyteDisplay` must be a list of 64 `String` strings"
 
 -- |Encrypt or decrypt a message with a single 16 bytes key resulting in a list of the same length.
 cryptBlockV1 :: [Word32] -> [Word32] -> [Word32] -> Int -> [Word32]
-cryptBlockV1 (x:xs) k n index = xor x (keybyte (cryptV1 k n index) (index `mod` 64)) :
-    cryptBlockV1 xs k n (index+1)
+cryptBlockV1 (x:xs) key nonce index
+    | length key == 16 && length nonce == 8 = xor x (keybyte (cryptV1 key nonce index) (index `mod` 64)) :
+        cryptBlockV1 xs key nonce (index+1)
+    | otherwise = error "first input to `cryptBlockV1` must be a list of 16 `Word32` numbers and the second a list of 8 `Word32` numbers"
 cryptBlockV1 _ _ _ _ = []
 
 -- |Encrypt or decrypt a message with a single 16 bytes key resulting in a list of the same length.
 cryptBlockV1Display :: [String] -> [String] -> [String] -> Int -> [String]
-cryptBlockV1Display (x:xs) k n index = printf "%s ⊕ %s" x (keybyteDisplay (cryptV1Display k n (show index)) (index `mod` 64)) :
-    cryptBlockV1Display xs k n (index+1)
+cryptBlockV1Display (x:xs) key nonce index
+    | length key == 16 && length nonce == 8 = printf "%s ⊕ %s" x (keybyteDisplay (cryptV1Display key nonce (show index)) (index `mod` 64)) :
+        cryptBlockV1Display xs key nonce (index+1)
+    | otherwise = error "first input to `cryptBlockV1Display` must be a list of 16 `String` strings and the second a list of 8 `String` strings"
 cryptBlockV1Display _ _ _ _ = []
 
 -- |Display the output of `cryptBlockV1` as string equations.
 cryptBlockV1Equations :: [String] -> [String] -> [String] -> Int -> [String]
-cryptBlockV1Equations m k n index = do
-    let display = cryptBlockV1Display m k n index
-    let displayIndex = zip [0 :: Int ..] display
-    let equation = map (uncurry (printf "c%d = %s")) displayIndex
-    equation
+cryptBlockV1Equations message key nonce index
+    | length key == 16 && length nonce == 8 =
+        [printf "z%d = %s" (idx :: Int) eq | (idx, eq) <- zip [0..] (cryptBlockV1Display message key nonce index)]
+    | otherwise = error "first input to `cryptBlockV1Equations` must be a list of 16 `String` strings and the second a list of 8 `String` strings"
 
 -- |Encrypt or decrypt a message with two 16 bytes key resulting in a list of the same length.
 cryptBlockV2 :: [Word32] -> [Word32] -> [Word32] -> [Word32] -> Int -> [Word32]
-cryptBlockV2 (x:xs) k0 k1 n index = xor x (keybyte (cryptV2 k0 k1 n index) (index `mod` 64)) :
-    cryptBlockV2 xs k0 k1 n (index+1)
+cryptBlockV2 (x:xs) key0 key1 nonce index
+    | length key0 == 16 && length key1 == 16 && length nonce == 8 =
+        xor x (keybyte (cryptV2 key0 key1 nonce index) (index `mod` 64)) :
+            cryptBlockV2 xs key0 key1 nonce (index+1)
+    | otherwise = error "first input to `cryptBlockV2` must be a list of 16 `Word32` numbers, the second a list of 16 `Word32` numbers and the third a list of 8 `Word32` numbers"
 cryptBlockV2 _ _ _ _ _ = []
